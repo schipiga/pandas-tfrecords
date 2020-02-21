df0794a0e4ef303bf1e8f3a68cdfed69f5e51e92
Sergei Chipiga <sergei.chipiga@pipedrive.com>
2020-02-21 05:58:17 +0200

    Add license

--- pandas_tfrecords/to_tfrecords.py
+++ pandas_tfrecords/to_tfrecords.py
@@ -131,8 +131,10 @@ def _bytes_feature(value):
         value = str.encode(value)
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
+
 def _float_feature(value):
     return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+
 
 def _int64_feature(value):
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
--- setup.py
+++ setup.py
@@ -3,12 +3,19 @@ from setuptools import setup
 
 setup(
     name='pandas-tfrecords',
-    version='0.1.1',
+    version='0.1.2',
     description='Converter pandas to tfrecords & tfrecords to pandas',
     long_description=open('README.rst').read(),
     url='https://github.com/schipiga/pandas-tfrecords/',
     author='Sergei Chipiga',
     author_email='chipiga86@gmail.com',
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
     packages=['pandas_tfrecords'],
     install_requires=[
         's3fs==0.4.0',
